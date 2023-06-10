fb48dd3199f1fd3041b582b2010d95a7977a83d5
DESKTOP-junto <juntou1230@gmail.com>
2023-06-10 18:23:44 +0900

    update: ファイル構成

--- dist/process/copy.js
+++ dist/process/copy.js
@@ -1,5 +1,5 @@
 "use strict";
 const Process_Script = require('fs').readFileSync(`./src/script.ts`, "utf8").replace("./src/input.txt", "/dev/stdin").split('\n');
-require('fs').writeFileSync('./src/process/output.txt', Process_Script.join("\n"));
-console.log("Copy complete!");
+require('fs').writeFileSync('./src/output.txt', Process_Script.join("\n"));
+console.log("Build complete!");
 //# sourceMappingURL=copy.js.map
