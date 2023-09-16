9873f5fe33e453cc304b13edd6cbd11f0d7319d3
DESKTOP-junto <juntou1230@gmail.com>
2023-09-16 19:32:27 +0900

    update: 関数削除

--- src/script.ts
+++ src/script.ts
@@ -1,32 +1,5 @@
-const isPrime = (num: number | bigint): boolean => {
-  if(typeof num == "number") {
-    for(let i = 2; i * i <= num; i++ ) {
-     if(num % i == 0) return false;
-    }
-  }else {
-    for(let i = 2n; i * i <= num; i++ ) {
-      if(num % i == 0n) return false;
-     }
-  }
-  return true;
-}
-const Gcd = (a: number, b: number): number => {
-    let r = a % b;
-    while(r != 0) {
-      a = b;
-      b = r;
-      r = a % b;
-    }
-    return b;
-}
-const Icm = (a: number, b: number): number => {
-  return a / Gcd(a, b) * b;
-}
 const SplitNumber = (e: string): number[] => {
   return e.split(" ").map(Number)
-}
-const SplitString = (e: string): string[] => {
-  return e.trim().split(" ");
 }
 
 const Main = (input: string[]) => {
