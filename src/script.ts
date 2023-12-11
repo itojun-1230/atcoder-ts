1fb055ca61c196cb9a6ee2f1b6106136f555ee19
DESKTOP-junto <juntou1230@gmail.com>
2023-12-11 23:03:38 +0900

    add: heapq

--- src/script.ts
+++ src/script.ts
@@ -29,6 +29,7 @@ reader.on("close", () => {
   finish();
 });
 
+
 const next = (): string => input[input_current++].trim();
 const nextNum = (): number => Number(input[input_current++]);
 const nextBigInt = (): BigInt => BigInt(input[input_current++]);
@@ -37,7 +38,76 @@ const nextNums = (length: number): number[] => [...new Array(length)].map(() => 
 const nextBigInts = (length: number): BigInt[] => [...new Array(length)].map(() => BigInt(input[input_current++]));
 const SpN = (e: string): number[] => e.split(" ").map(Number);
 
-const print = (out: number | BigInt | string) => outputLine.push(String(out));
+const print = <T>(out: T) => outputLine.push(String(out));
+
+class heapq<T> {
+  _data: {
+    p: number,
+    d: T
+  }[] = [];
+  private _size: number = 0;
+
+  enqueue = (priority: number, data: T) => {
+    this._data.push({
+      p: priority,
+      d: data
+    });
+    if (this._size > 0) {
+      let parent = Math.floor((this._size - 1) / 2);
+      let index = this._size;
+      while (parent >= 0) {
+        if (this._data[parent].p < this._data[index].p) {
+          [this._data[parent], this._data[index]] = swap(this._data[parent], this._data[index]);
+          index = parent;
+          parent = Math.floor((index - 1) / 2);
+        } else {
+          break;
+        }
+      }
+    }
+    this._size++;
+  }
+  dequeue = () => {
+    if (this._size == 0) {
+      return undefined;
+    }
+    this._size--;
+
+    let heap = this._data;
+    const result = heap[0];
+    heap[0] = heap.pop()!;
+
+    let index = 0;
+    let left = 1, right = 2;
+    while (left < this._size) {
+      if (right >= this._size) {
+        if (heap[index].p < heap[left].p) {
+          [heap[index], heap[left]] = swap(heap[index], heap[left]);
+        }
+        break;
+      }
+      if (heap[left].p < heap[right].p && heap[index].p < heap[right].p) {
+        [heap[index], heap[right]] = swap(heap[index], heap[right]);
+        index = right;
+      } else if (heap[index].p < heap[left].p) {
+        [heap[index], heap[left]] = swap(heap[index], heap[left]);
+        index = left;
+      } else {
+        break;
+      }
+      left = Math.floor(index * 2) + 1;
+      right = Math.floor(index * 2) + 2;
+    }
+
+    return result;
+  }
+  top = () => {
+    return this._data[0];
+  }
+  size = () => {
+    return this._size;
+  }
+}
 
 const Deep = <T>(e: T): T => structuredClone(e);  //DeepCopy
 const UnArr = (n: number): undefined[] => [...new Array(n)];  //unfefined Array
